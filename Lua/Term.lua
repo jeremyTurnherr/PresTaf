69674f518ded03554790bf75cc488c55b75dfa2c
Tengu <maxime.roque@etu.univ-orleans.fr>
2017-05-15 13:15:18 +0200

    term 1

--- Lua/Term.lua
+++ Lua/Term.lua
@@ -38,14 +38,79 @@ end
 
 function Term.neg(self)
 	negCoef={}
-	print("-----")
-	for i=1,self:nbVar() do
+	
+	for i=1,(self:nbVar()) do
+		print("-"..self.coef[i])
 		negCoef[i]=-self.coef[i]
 	end
 	print("-----")
 	return Term.new("-"..self.expr,self.var,negCoef,-self.constant)
-	--~ return 0
 end
+
+function Term.plus(self,t) 
+	plusvar=merge(self.var,t.var)
+	pluscoef={}
+	for i=1,len(plusvar) do
+		pluscoef[i]=0
+	end
+	k=1
+	i=1
+	print("--1--")
+	while i<=len(plusvar) and k<=self:nbVar() do
+		if plusvar[i]==self.var[i] then
+			pluscoef[i]=self.coef[i]
+			k=k+1
+			
+		end
+		i=i+1
+	end
+	print("--1--")
+	
+	while i<=len(plusvar) and k<=t:nbVar() do
+		if plusvar[i]==t.var[i] then
+			pluscoef[i]=pluscoef[i]+t.coef[i]
+			k=k+1
+			
+		end
+		i=i+1
+	end
+	print("--1--")
+	return Term.new(self.expr.." + "..t.expr,plusvar,pluscoef,self.constant+t.constant)
+	--~ return Term.new(" + ",plusvar,pluscoef,self.constant+t.constant)
+	
+end
+
+function Term.minus(self,t) 
+	plusvar=merge(self.var,t.var)
+	pluscoef={}
+	for i=1,len(plusvar) do
+		pluscoef[i]=0
+	end
+	k=1
+	i=1
+	while i<=len(plusvar) and k<=self:nbVar() do
+		if plusvar[i]==self.var[i] then
+			pluscoef[i]=self.coef[i]
+			k=k+1
+			
+		end
+		i=i+1
+	end
+	
+	while i<=len(plusvar) and k<=t:nbVar() do
+		if plusvar[i]==t.var[i] then
+			pluscoef[i]=pluscoef[i]-t.coef[i]
+			k=k+1
+			
+		end
+		i=i+1
+	end
+	return Term.new(self.expr.." + "..t.expr,plusvar,pluscoef,self.constant-t.constant)
+	
+end
+	
+
+
 
 function len(tab)
 	Count = 0
@@ -57,25 +122,65 @@ end
 
 
 function merge(a,b) -- deux listes de int
-    n=0
-    ai=0
-    bi=0
     lena=len(a)
     lenb=len(b)
-    while ai<lena and bi < lenb do
-        if a[ai] == b[bi] then
-            ai=ai+1
-            bi=bi+1
-            n=n+1
-        else if a[ai]<b[bi] then
+    res={}
+    n=1
+    ai=1
+    bi=1
+    while ai<=lena and bi<=lenb do
+		if a[ai] == b[bi] then
+			res[n]=a[ai]
+			ai=ai+1
+			bi=bi+1
+			n=n+1
+		else if a[ai] <	b[bi] then
+				res[n]=a[ai]
 				ai=ai+1
 				n=n+1
-			else
+			else 
+				res[n]=b[bi]
 				bi=bi+1
 				n=n+1
 			end
 		end
     end
+    while ai<=lena do
+		res[n]=a[ai]
+		ai=ai+1
+		n=n+1
+	end
+	while bi<=lenb do
+		res[n]=b[bi]
+		bi=bi+1
+		n=n+1
+	end
+	return res
+end
+
+function getIndex(v)
+    o = Term.map[v]
+    if o ~= nil then
+        return o
+    else
+        Term.map[v] = Term.size
+        Term.size = Term.size + 1
+        return Term.size - 1
+    end
+end
+
+function factor(a, v)
+    index = getIndex(v)
+    return Term.new(a .. "*" .. v, {index}, {a}, 0)
+end
+
+function variable(v)
+    index = getIndex(v)
+    return Term.new(v, {index}, {1}, 0)
+end
+
+function integer(a)
+    return Term.new(a, {}, {}, a)
 end
 
 
@@ -85,7 +190,15 @@ Term.size=0
 
 local module={}
 
+Term.map = {}
+
 module.Term=Term
 module.merge=merge
+
+module.getIndex = getIndex
+module.map = map
+module.factor = factor
+module.variable = variable
+module.integer = integer
 
 return module
--- Lua/testInterface.lua
+++ Lua/testInterface.lua
@@ -3,22 +3,44 @@ term=require('Term')
 print(prestaf)
 
 
+function printab(tab)
+	res="{"
+	for k,i in pairs(tab) do
+		res=res..k..":   "..i.."  ,"
+	end
+	print(res.."}")
+end
+
+
 
 print("-")
 function run(pres)
 	local i=term.Term.new("exp",{1,2,3},{2,2,2},5)
 	print("-")
+	
 	print(i:nbVar())
 	print(i.var[2])
 	print("-")
 	print(i:debug())
-	print("-")
-	print(term.merge({1,2,3},{3,2,1}))
+	print("-merge-")
+	printab(term.merge({1,2,3},{3,2,1}))
 	print("-")
 	print(term.Term.size)
-	print("--")
-	print(i:neg():debug())
-	print("-")
+	print("-neg-")
+	local y=i:neg()
+	print(y:debug())
+	print("-plus-")
+	local z=y:plus(i)
+	print(z:debug())
+	print("-minus-")
+	print(z:minus(i):debug())
+	print("-factor 9-")
+	print(term.factor(9,"expppp"):debug())
+	print("-variable-")
+	print(term.variable("expppp"):debug())
+	print("-integer-")
+	print(term.integer(42):debug())
+
 	
 	
 	print("ji endo")
