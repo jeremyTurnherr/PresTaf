0d8ed831cf44257547759a155e02c6005122b6e6
Tengu <maxime.roque@etu.univ-orleans.fr>
2017-05-19 14:27:36 +0200

    +automates java

---
+++ Lua/Automaton.lua
@@ -0,0 +1,47 @@
+
+
+function len(tab)
+	local Count = 0
+	for k,i in pairs(tab) do
+	  Count = Count + 1
+	end
+	return Count
+end
+
+function fillsuccparam(succ)
+
+	prestaf:init_succ(len(succ),len(succ[1]))
+	for postate,state in pairs(b) do
+		for posletter,t in pairs(state) do
+			prestaf:fillsucc(postate,posletter,t)
+		end
+	end
+
+end
+
+function fillfinalparam(b)
+
+	prestaf:init_final(len(b))
+	for pos,val in pairs(b) do
+	  prestaf:fillfinal(pos,val)
+	end
+
+end
+
+function createAutomaton(initial,succ,finals)
+	fillsuccparam(succ)
+	fillfinalparam(finals)
+	return prestaf:createAutomaton(initial)
+	
+end
+	
+
+
+
+
+local module={}
+
+
+module.createAutomaton=createAutomaton
+
+return module
